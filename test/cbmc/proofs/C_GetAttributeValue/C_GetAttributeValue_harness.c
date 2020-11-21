241d36d8966fb409d441a423987f60663f164585
Carl Lundin <lundinc@amazon.com>
2020-11-20 18:03:04 -0800

    Added CBMC proof for C_GetAttributeVale.

--- test/cbmc/proofs/C_GetAttributeValue/C_GetAttributeValue_harness.c
+++ test/cbmc/proofs/C_GetAttributeValue/C_GetAttributeValue_harness.c
@@ -31,55 +31,76 @@
 #include <stddef.h>
 #include "mbedtls/ecp.h"
 #include "mbedtls/oid.h"
+#include "mbedtls/sha256.h"
+#include "mbedtls/pk.h"
+#include "core_pkcs11_config.h"
 #include "core_pkcs11.h"
 
-#define EC_D_LENGTH 32
-#define EC_PARAMS_LENGTH 10
+/* Internal struct for corePKCS11 mbed TLS implementation, but we don't really care what it contains
+ * in this proof.
+ *
+ * It is just copied over from "core_pkcs11_mbedtls.c" so the structure is correct.
+ */
+typedef struct P11Session
+{
+    CK_ULONG ulState;                            
+    CK_BBOOL xOpened;                            
+    CK_MECHANISM_TYPE xOperationDigestMechanism; 
+    CK_BYTE * pxFindObjectLabel;                 
+    CK_ULONG xFindObjectLabelLen;                
+    CK_MECHANISM_TYPE xOperationVerifyMechanism; 
+    mbedtls_threading_mutex_t xVerifyMutex;      
+    CK_OBJECT_HANDLE xVerifyKeyHandle;           
+    mbedtls_pk_context xVerifyKey;               
+    CK_MECHANISM_TYPE xOperationSignMechanism;   
+    mbedtls_threading_mutex_t xSignMutex;        
+    CK_OBJECT_HANDLE xSignKeyHandle;             
+    mbedtls_pk_context xSignKey;                 
+    mbedtls_sha256_context xSHA256Context;       
+} P11Session_t;
+ 
+CK_RV __CPROVER_file_local_core_pkcs11_mbedtls_c_prvCheckValidSessionAndModule( const P11Session_t * pxSession )
+{
+    __CPROVER_assert( pxSession != NULL, "pxSession was NULL." );
+    return CKR_OK;
+}
 
-#define EC_PRIV_KEY_INITIALIZER                                                            \
-    {                                                                                      \
-        { CKA_CLASS, &xPrivateKeyClass, sizeof( CK_OBJECT_CLASS ) },                       \
-        { CKA_KEY_TYPE, &xPrivateKeyType, sizeof( CK_KEY_TYPE ) },                         \
-        { CKA_LABEL, pucPrivLabel, ( CK_ULONG ) sizeof( pucPrivLabel ) }, \
-        { CKA_TOKEN, &xTrue, sizeof( CK_BBOOL ) },                                         \
-        { CKA_SIGN, &xTrue, sizeof( CK_BBOOL ) },                                          \
-        { CKA_EC_PARAMS, pxEcPrivParams, EC_PARAMS_LENGTH },                               \
-        { CKA_VALUE, pxD, EC_D_LENGTH }                                                    \
-    }
+void  __CPROVER_file_local_core_pkcs11_mbedtls_c_prvFindObjectInListByHandle( CK_OBJECT_HANDLE xAppHandle,
+                                         CK_OBJECT_HANDLE_PTR pxPalHandle,
+                                         CK_BYTE_PTR * ppcLabel,
+                                         CK_ULONG_PTR pxLabelLength )
+{
+    __CPROVER_assert( pxPalHandle != NULL, "ppcLabel was NULL." );
+    __CPROVER_assert( ppcLabel != NULL, "ppcLabel was NULL." );
+    __CPROVER_assert( pxLabelLength != NULL, "ppcLabel was NULL." );
+
+    /* The value of the PAL handle doesn't matter as long as it is not 0, which is considered
+     * an invalid handle by the PKCS #11 specification.
+     */
+    *pxPalHandle = 1;
+}
 
 void harness()
 {
-    
     CK_SESSION_HANDLE hSession;
     CK_OBJECT_HANDLE xObject;
     CK_ULONG ulCount;
-    CK_KEY_TYPE xPrivateKeyType = CKK_EC;
-    CK_OBJECT_CLASS xPrivateKeyClass = CKO_PRIVATE_KEY;
-    CK_BBOOL xTrue = CK_TRUE;
-    CK_FLAGS xFlags;
-    char * pucPrivLabel = "Priv key label";
-    /* DER-encoding of an ANSI X9.62 Parameters value */
-    CK_BYTE * pxEcPrivParams = ( CK_BYTE * ) ( "\x06\x08" MBEDTLS_OID_EC_GRP_SECP256R1 );
     CK_RV xResult;
 
-    /* Private value D. */
-    CK_BYTE pxD[ EC_D_LENGTH ] = { 0 };
-
-    CK_ATTRIBUTE xPrivateKeyTemplate[] = EC_PRIV_KEY_INITIALIZER;
-    xResult = C_Initialize( NULL );
-    __CPROVER_assume( xResult == CKR_OK );
-
-    xResult = C_OpenSession( 0, xFlags, NULL, 0, &hSession );
-    __CPROVER_assume( xResult == CKR_OK );
-
-    xResult = C_CreateObject( hSession,
-                                  ( CK_ATTRIBUTE_PTR ) &xPrivateKeyTemplate,
-                                  sizeof( xPrivateKeyTemplate ) / sizeof( CK_ATTRIBUTE ),
-                                  &xObject );
-    __CPROVER_assume( xResult == CKR_OK );
-
-    __CPROVER_assume( ulCount < 10 );
+    __CPROVER_assume( ulCount <= TEMPLATE_SIZE && ulCount >= 1 );
     CK_ATTRIBUTE_PTR pTemplate = malloc( sizeof( CK_ATTRIBUTE ) * ulCount );
+    __CPROVER_assume( pTemplate != NULL );
 
+    for(int i = 0; i< ulCount; i++)
+    {
+        __CPROVER_assume( pTemplate[i].ulValueLen <= 256 );
+        pTemplate[i].pValue = malloc( pTemplate[i].ulValueLen );
+    }
+
+
+    /* 0 is an invalid session handle according to the spec. 
+     * This assumption is checked in prvSessionPointerFromHandle.
+     */
+    __CPROVER_assume( hSession >= 1 && hSession <= pkcs11configMAX_SESSIONS );
   ( void ) C_GetAttributeValue( hSession, xObject, pTemplate, ulCount );
 }
